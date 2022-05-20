79ea87662c8afe4ebce6171d33c28e97d2ee841f
a.rudzik <a.rudzik@ibemag.onmicrosoft.com>
2022-05-20 11:19:13 +0200

    NewUser navigation added

--- prakt_ScreenShare/View/NewUserWindow.xaml.cs
+++ prakt_ScreenShare/View/NewUserWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using prakt_ScreenShare.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using prakt_ScreenShare.Model;
+using System.Diagnostics;
 
 namespace prakt_ScreenShare.View
 {
@@ -20,6 +23,7 @@ namespace prakt_ScreenShare.View
     /// </summary>
     public partial class NewUserWindow : Window
     {
+        DataBaseService db = new DataBaseService();
         public NewUserWindow()
         {
             InitializeComponent();
@@ -28,6 +32,18 @@ namespace prakt_ScreenShare.View
         {
             Regex regex = new Regex("[^0-9]+");
             e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) //Kliknięto zapisz
+        {
+            UserEntries user = new UserEntries() {Name = UserName_txt.Text, IP = $"{IP_1.Text}.{IP_2.Text}.{IP_3.Text}.{IP_4.Text}"}; //Obiekt User
+            db.AddUser(user);
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)//Kliknięto Anuluj
+        {
+            this.Close();
         }
     }
 }
---
+++ prakt_ScreenShare/View/UsersWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace prakt_ScreenShare.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy UsersWindow.xaml
+    /// </summary>
+    public partial class UsersWindow : Window
+    {
+        public UsersWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            NewUserWindow newuser = new NewUserWindow();
+            newuser.Show();
+        }
+    }
+}
